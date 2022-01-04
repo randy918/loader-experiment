31a375d71324b8824651cb7f735fbfc40b8c101d
randy918 <randy918@gmail.com>
2022-01-04 09:59:28 -0600

    tailwind installed

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@ function main() {
 window.onload = function () {
   main();
 };
+c("hello");
+
+//c(job);
+//c(year);
+
+var me = "johns";
+let job = "junior exec";
+const year = 1941;
+
+c(addNumbers(8, 7));
+c(addDecl(8, 7));
+c(addStuff(7, 6));
+
+const addDecl = (a, b) => a + b;
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+const addStuff = function (a, b) {
+  return a + b;
+};
